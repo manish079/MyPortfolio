870323bddf9ac1f09806e98f8cc193d24ad197d1
manish079 <manishprajapat492@gmail.com>
2023-05-29 18:31:38 +0530

    added nav-links smooth scroll

--- script.js
+++ script.js
@@ -1,63 +1,40 @@
-var viewResume = document.querySelector("#resume");
+// Variables
+const viewResume = document.querySelector("#resume");
+const p_btns = document.querySelector(".p-btns");
+const p_btn = document.querySelectorAll(".p-btn");
+const p_img_elem = document.querySelectorAll(".img-overlay");
+const lightTheme = document.querySelector(".light-theme");
+const darkTheme = document.querySelector(".dark-theme");
 
+// Resume View
 viewResume.addEventListener("click", function () {
   viewResume.addEventListener("click", function () {
     //alert("Hello")
   });
 });
 
-//nav button
-// document.querySelector(".nav-links").classList.toggle("nav-links-active");
-// document.querySelector("#nav-toggle").addEventListener("click", () => {
-//   document.querySelector(".active").style.left = 0;
-
-//   document
-//     .querySelector(".nav-links-active .secondary-button")
-//     .classList.remove("secondary-button");
-
-//   document
-//     .querySelector(".nav-links-active .primary-button")
-//     .classList.remove("primary-button");
-// });
-
-// creating portfolio project tab component
-
-const p_btns = document.querySelector(".p-btns");
-const p_btn = document.querySelectorAll(".p-btn");
-const p_img_elem = document.querySelectorAll(".img-overlay");
-
+// Portfolio project tab component
 p_btn[0].classList.add("p-active-btn");
 
 p_btns.addEventListener("click", (event) => {
-  const p_btn_clicked = event.target; //current tab
-  // console.log(p_btn_clicked);
+  const p_btn_clicked = event.target; // Current tab
 
   if (p_btn_clicked !== p_btns) {
     p_btn.forEach((currEle) => currEle.classList.remove("p-active-btn"));
-    // if (p_btn_clicked != p_btns) {
     p_btn_clicked.classList.add("p-active-btn");
 
-    // to find the number in data Attr
+    // Find the number in data attribute
     const btn_num = p_btn_clicked.dataset.btnNum;
-    // console.log(btn_num);
 
     const img_active = document.querySelectorAll(`.p-btn--${btn_num}`);
 
     p_img_elem.forEach((currEle) => currEle.classList.add("hidden"));
-
     img_active.forEach((currEle) => currEle.classList.remove("hidden"));
   }
 });
 
-// =====================================================================================
 // Dark and Light Theme
-
-document.querySelector(".light-theme").addEventListener("click", () => {
-  // console.log("Click");
-
-  const lightTheme = document.querySelector(".light-theme");
-  const darkTheme = document.querySelector(".dark-theme");
-
+lightTheme.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode-class");
   document.querySelector("footer").style.backgroundColor = "#101010";
   document.querySelector(".coding-card-item h5").style.color = "#14213d";
@@ -67,31 +44,25 @@ document.querySelector(".light-theme").addEventListener("click", () => {
   lightTheme.style.display = "none";
   darkTheme.style.display = "block";
 });
-document.querySelector(".dark-theme").addEventListener("click", () => {
-  const lightTheme = document.querySelector(".light-theme");
-  const darkTheme = document.querySelector(".dark-theme");
 
+darkTheme.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode-class");
-  document.querySelector("footer").style.backgroundColor = ""; // Restore default value
-  document.querySelector(".coding-card-item h5").style.color = ""; // Restore default value
-  document.querySelector(".coding-card-item a").style.color = ""; // Restore default value
+  document.querySelector("footer").style.backgroundColor = "";
+  document.querySelector(".coding-card-item h5").style.color = "";
+  document.querySelector(".coding-card-item a").style.color = "";
 
   // Toggle visibility of light-theme and dark-theme icons
   lightTheme.style.display = "block";
   darkTheme.style.display = "none";
 });
 
-// =====================================================================================
-//Scroll page bottom to top when click on scroll-up-btn
-
-//When user scrolls down 20px from the top of the document, show the btn
-
+// Scroll page bottom to top when click on scroll-up-btn
 window.onscroll = function () {
   scrollFunction();
 };
 
 function scrollFunction() {
-  var scrollToTopButton = document.querySelector(".scroll-up-btn");
+  const scrollToTopButton = document.querySelector(".scroll-up-btn");
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     scrollToTopButton.style.display = "block";
   } else {
@@ -105,16 +76,10 @@ document.querySelector(".scroll-up-btn").onclick = function () {
 };
 
 function scrollToTop() {
-  //Without transition it goes to top
+  const scrollToTopButton = document.querySelector(".scroll-up-btn");
+  const scrollStep = -window.scrollY / (500 / 15); // Adjust the scroll speed here
 
-  /*  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0; */
-
-  //With transition
-  var scrollToTopButton = document.querySelector(".scroll-up-btn");
-  var scrollStep = -window.scrollY / (500 / 15); // Adjust the scroll speed here
-
-  var scrollInterval = setInterval(function () {
+  const scrollInterval = setInterval(function () {
     if (window.scrollY !== 0) {
       window.scrollBy(0, scrollStep);
     } else {
@@ -122,3 +87,24 @@ function scrollToTop() {
     }
   }, 15); // Adjust the interval speed here
 }
+
+//Transition Scroll when click on nav-links
+const navLinks = document.querySelectorAll(".nav-links ul a");
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    // Toggle the active class on the clicked link
+    navLinks.forEach((link) => link.classList.remove("active"));
+    link.classList.add("active");
+
+    // Get the target section from the link's href attribute
+    const targetSection = document.querySelector(link.getAttribute("href"));
+
+    // Scroll to the target section with smooth behavior
+    targetSection.scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
